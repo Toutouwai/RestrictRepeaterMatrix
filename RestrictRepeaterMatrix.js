370a087eab7996dc1af44f453539e3dda5eff69e
Robin Sallis <Toutouwai@users.noreply.github.com>
2019-12-21 12:32:43 +1300

    v0.1.5
    Fix for nested Repeater Matrix fields.

--- RestrictRepeaterMatrix.js
+++ RestrictRepeaterMatrix.js
@@ -4,16 +4,16 @@ function checkRepeaterMatrix(inputfield_class, matrix_type, limit) {
 	if( !(inputfield_class && matrix_type && limit) ) return;
 
 	// Get repeater items of this matrix type
-	var $repeater_items = $(inputfield_class + ' .InputfieldRepeaterItem[data-type="' + matrix_type + '"]').not('.InputfieldRepeaterNewItem');
-	
+	var $repeater_items = $(inputfield_class + ' > .InputfieldContent > .Inputfields > .InputfieldRepeaterItem[data-type="' + matrix_type + '"]').not('.InputfieldRepeaterNewItem');
+
 	// Get repeater items not of this matrix type
-	var $other_repeater_items = $(inputfield_class + ' .InputfieldRepeaterItem[data-type!="' + matrix_type + '"]').not('.InputfieldRepeaterNewItem');
+	var $other_repeater_items = $(inputfield_class + ' > .InputfieldContent > .Inputfields > .InputfieldRepeaterItem[data-type!="' + matrix_type + '"]').not('.InputfieldRepeaterNewItem');
 
 	// Check if limit has been reached
-	var $add_link = $(inputfield_class + ' > div > .InputfieldRepeaterMatrixAddItem .InputfieldRepeaterMatrixAddLink[data-type="' + matrix_type + '"]');
+	var $add_link = $(inputfield_class + ' > .InputfieldContent > .InputfieldRepeaterMatrixAddItem .InputfieldRepeaterMatrixAddLink[data-type="' + matrix_type + '"]');
 	if(limit === 0 || $repeater_items.length > limit - 1) {
 		// Remove type from settings select
-		$other_repeater_items.find('select[id^=Inputfield_type_repeater] option[value=' + matrix_type + ']').remove();
+		$other_repeater_items.find('> .InputfieldContent > .Inputfields > .InputfieldSelect select[id^=Inputfield_type_repeater] option[value=' + matrix_type + ']').remove();
 		// Hide add link
 		$add_link.removeClass('rrm-show');
 	} else {
@@ -22,7 +22,7 @@ function checkRepeaterMatrix(inputfield_class, matrix_type, limit) {
 	}
 
 	// Hide add-items container if no visible child links
-	var $add_items = $(inputfield_class + ' > div > .InputfieldRepeaterMatrixAddItem');
+	var $add_items = $(inputfield_class + ' > .InputfieldContent > .InputfieldRepeaterMatrixAddItem');
 	$add_items.show();
 	var $add_links = $add_items.find('a');
 	var $hidden_add_links = $add_links.filter(function() {
